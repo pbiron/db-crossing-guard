8d6f708f7aeb4a98292c89c00a1f770670b8bda7
Paul Biron <paul@sparrowhawkcomputing.com>
2024-11-22 16:45:55 -0700

    Coding Standards: update our Base class to what we use in other projects.

--- includes/class-base.php
+++ includes/class-base.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Base class.
+ * Base class
  *
- * @since 0.2.3
+ * @since 0.1.0
  *
  * @package db-crossing-guard
  */
@@ -14,22 +14,20 @@ defined( 'ABSPATH' ) || die;
 /**
  * Abstract base class for all other classes.
  *
- * @since 0.2.3
- *
- * @phpstan-consistent-constructor
+ * @since 0.1.0
  */
 abstract class Base {
 	/**
 	 * Constructor.
 	 *
-	 * @since 0.2.3
+	 * @since 0.1.0
 	 *
 	 * @param mixed ...$args Optional arguments.  Declaring this here with the spread operator
 	 *                       allows sub-classes to declare specific arguments.
 	 */
 	public function __construct( ...$args ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
-		if ( method_exists( $this, '_setup' ) ) {
-			$this->_setup();
+		if ( method_exists( $this, 'setup' ) ) {
+			$this->setup();
 		}
 
 		$this->add_hooks();
@@ -40,7 +38,7 @@ abstract class Base {
 	 *
 	 * Sublcasses that override this method **must** call `parent::add_hooks()`.
 	 *
-	 * @since 0.2.3
+	 * @since 0.1.0
 	 *
 	 * @return void
 	 */
@@ -53,7 +51,7 @@ abstract class Base {
 			'after_setup_theme'     => 'after_setup_theme',
 			'init'                  => array( 'init', 'register_scripts', 'register_styles', 'register_block_types' ),
 			'admin_enqueue_scripts' => array( 'admin_enqueue_scripts', 'admin_enqueue_styles' ),
-			'wp_enqueue_scripts'    => 'wp_enqueue_scripts',
+			'wp_enqueue_scripts'    => array( 'wp_enqueue_scripts', 'enqueue_styles' ),
 			'cli_init'              => 'cli_init',
 		);
 		foreach ( $hooks as $hook => $methods ) {
@@ -64,6 +62,32 @@ abstract class Base {
 			}
 		}
 
+		return;
+	}
+
+	/**
+	 * Perform cleanup when our plugin is uninstalled.
+	 *
+	 * Subclasses that override this method, must call parent::ininstall().
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return void
+	 */
+	public static function uninstall() {
+		return;
+	}
+
+	/**
+	 * Unschedule our cron job when we're deactivated.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param bool $network_wide Optional. Whether the plugin was deactivated network wide.  Default false.
+	 *
+	 * @return void
+	 */
+	public function deactivate( $network_wide = false ) { // phpcs:ignore Generic.CodeAnalysis.UnusedFunctionParameter.Found
 		return;
 	}
 }
